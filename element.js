7a37ce3a02adbc234406f339ae75755edaa4c540
minhahn <chaness1000@gmail.com>
2018-11-07 20:00:44 -0600

    Update element.js
    Added empty prototype to be filled in asap.

--- element.js
+++ element.js
@@ -15,6 +15,11 @@ function Element(pos, url, scl, spd){
 
 }
 
+//empty constructor. void
+Element.prototype.Element = function(){
+    //create element with pos = , url = https://scontent-ort2-2.xx.fbcdn.net/v/t1.15752-9/45553256_1847037375409918_2529009054647320576_n.png?_nc_cat=107&_nc_ht=scontent-ort2-2.xx&oh=c959df2ca02e1b820aae869c12b4b978&oe=5C743003
+    // scale = , speed = 
+}  
 
 
 /* Example for how to create a function for a prototype. this is the javascript version
