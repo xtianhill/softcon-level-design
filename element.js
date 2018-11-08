270a877eabae087ddf9317db68de65ab69aa5464
minhahn <chaness1000@gmail.com>
2018-11-07 20:03:55 -0600

    element.js
    Added more prototype outlines, to be filled in asap

--- element.js
+++ element.js
@@ -19,7 +19,28 @@ function Element(pos, url, scl, spd){
 Element.prototype.Element = function(){
     //create element with pos = , url = https://scontent-ort2-2.xx.fbcdn.net/v/t1.15752-9/45553256_1847037375409918_2529009054647320576_n.png?_nc_cat=107&_nc_ht=scontent-ort2-2.xx&oh=c959df2ca02e1b820aae869c12b4b978&oe=5C743003
     // scale = , speed = 
-}  
+}
+
+//gets position. return position coordinates
+Element.prototype.getposition = function(){
+    //get position coordinates
+}
+
+//gets sprite. return sprite url
+Element.prototype.getsprite = function(){
+    //get sprite url
+}
+
+//gets scale. return scale dimensions
+Element.prototype.getscale = function(){
+    //get scale dimensions
+}
+
+//gets speed. return speed of x and y
+Element.prototype.getspeed = function(){
+    //get speed of x and y
+}
+
 
 
 /* Example for how to create a function for a prototype. this is the javascript version
