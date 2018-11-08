0dbc0915c8a487fb43d6d019ae6fd49d64829aca
Radhika Kaicker <radhikakaicker@Radhikas-MacBook-Pro.local>
2018-11-08 03:04:42 -0600

    item.js

--- item.js
+++ item.js
@@ -1,1 +1,17 @@
-  
+/* Item Prototype */
+function Item(collected){
+    this.collected = collected
+}
+
+Item.prototype.Item = function(){  
+
+};
+
+Item.prototype.collect = function(){
+ 	
+};
+
+Item.prototype.getCollected = function(){
+    
+};
+
