7010b59b94d8fb938846bb0ec9e51244e01ad012
Christian Hill <hill.cb24@gmail.com>
2018-11-10 17:08:36 -0600

    changed application file to test

--- flask/application.py
+++ flask/application.py
@@ -6,6 +6,7 @@ application.debug = True
 @application.route('/', methods=['GET'])
 def hello():
     return '<p>Hello world: this should be different now</p>'
+    return '<p>Hello world: two lines this time!</p>'
 
 if __name__ == "__main__":
     application.run()
