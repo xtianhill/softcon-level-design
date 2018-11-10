1737ae0091dab8ea7034c33f435d9b835924ee5f
Christian Hill <hill.cb24@gmail.com>
2018-11-10 17:52:01 -0600

    REST now working; can handle simple JSON

--- flask/application.py
+++ flask/application.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 application = Flask(__name__)
 application.debug = True
 
 @application.route('/', methods=['GET'])
 def hello():
-    return '<p>Hello world: this should be different now</p>'
-    return '<p>Hello world: two lines this time!</p>'
+    return 'wrong URL: try using .../api/v[n]/...'
+
+@application.route('/test', methods=['GET'])
+def test():
+    return 'Level Design backend is running'
+
+@application.route('/api/v1/add-grid/', methods=['POST'])
+def add_grid():
+    if not request.json:
+        return 'ERROR: invalid input'
+    ret = 'Valid JSON received\nJSON: ' + str(request.json)
+    return ret
+
+@application.route('/api/v1/search-grid/<title>', methods=['GET'])
+def search_grid(title):
+    return title
 
 if __name__ == "__main__":
     application.run()
