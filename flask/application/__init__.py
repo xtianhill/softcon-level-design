13ab0084d13b7925690424cc2f53dbfe734be701
Christian Hill <hill.cb24@gmail.com>
2018-11-10 18:16:47 -0600

    added updated requirements and application files

---
+++ flask/application/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+application = Flask(__name__)
+application.config.from_object('config')
+db = SQLAlchemy(application)
+print db.engine
