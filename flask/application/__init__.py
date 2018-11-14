5f274516935986f5d7f9a5ae34264c9bc5046148
Christian Hill <hill.cb24@gmail.com>
2018-11-13 21:19:43 -0600

    changed CORS stuff to localdomain

--- flask/application/__init__.py
+++ flask/application/__init__.py
@@ -3,7 +3,9 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
-cors = CORS(application, resources={r"/api/*": {"origins": "*"}})
+# cors = CORS(application, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(application, resources={r"/api/*": {"origins": "http://localhost:port"}})
+
 application.debug = True
 application.config.from_object('config')
 db = SQLAlchemy(application)
