6662cb74c2e17387e51600be8c8891d8611181a0
Christian Hill <hill.cb24@gmail.com>
2018-11-17 17:13:50 -0600

    changed all Flask routing stuff to return HTTP response instead of plain strings

--- flask/application.py
+++ flask/application.py
@@ -38,58 +38,59 @@ def send_js(path):
 @application.route('/api/v1/add-grid/', methods=['POST'])
 def add_grid():
     if not request.json:
-        return Response(request.form, status=HTTP_BADREQUEST)
-    if not validate_json(request.get_json()):
-        return Response(request.form, status=HTTP_BADREQUEST)
-    my_json = request.get_json()
-    my_title = my_json["title"]
-    my_data = my_json["data"]
-    my_grid = Grid(my_title, my_data)
-    try:
-        exists = db.session.query(Grid.title).filter_by(title=my_title).scalar() is not None
-        if exists:
-            raise exc.DataError("", "", "", False, 0)
-        db.session.add(my_grid)
-        db.session.commit()
-        response = Response(my_json, status=HTTP_CREATED, mimetype='application/json') 
-    except exc.DataError:
-        db.session.rollback()
-        response = Response(my_json, status=HTTP_CONFLICT, mimetype='application/json')
-    finally:
-        db.session.close()
+        response = Response(request.form, status=HTTP_BADREQUEST)
+    elif not validate_json(request.get_json()):
+        response = Response(request.form, status=HTTP_BADREQUEST)
+    else:
+        my_json = request.get_json()
+        my_title = my_json["title"]
+        my_data = my_json["data"]
+        my_grid = Grid(my_title, my_data)
+        try:
+            exists = db.session.query(Grid.title).filter_by(title=my_title).scalar() is not None
+            if exists:
+                raise exc.DataError("", "", "", False, 0)
+            db.session.add(my_grid)
+            db.session.commit()
+            response = Response(my_json, status=HTTP_CREATED, mimetype='application/json') 
+        except exc.DataError:
+            db.session.rollback()
+            response = Response(my_json, status=HTTP_CONFLICT, mimetype='application/json')
+        finally:
+            db.session.close()
     return response
 
 @application.route('/api/v1/update-grid/', methods=['POST'])
 def update_grid():
     if not request.json:
-        return Response(request.form, status=HTTP_BADREQUEST)
-    if not validate_json(request.get_json()):
-        return Response(request.form, status=HTTP_BADREQUEST)
-    my_json = request.get_json()
-    my_title = my_json["title"]
-    new_data = my_json["data"]
-    try:
-        exists = db.session.query(Grid.title).filter_by(title=my_title).scalar() is not None
-        if not exists:
-            raise exc.DataError("", "", "", False, 0)
-        my_grid = Grid.query.filter_by(title=my_title).first()
-        my_grid.data = new_data
-        db.session.commit()
-        response = Response(my_json, status=HTTP_CREATED, mimetype='application/json') 
-    except exc.DataError:
-        db.session.rollback()
-        response = Response(my_json, status=HTTP_CONFLICT, mimetype='application/json')
-    finally:
-        db.session.close()
+        response = Response(request.form, status=HTTP_BADREQUEST)
+    elif not validate_json(request.get_json()):
+        response = Response(request.form, status=HTTP_BADREQUEST)
+    else:
+        my_json = request.get_json()
+        my_title = my_json["title"]
+        new_data = my_json["data"]
+        try:
+            exists = db.session.query(Grid.title).filter_by(title=my_title).scalar() is not None
+            if not exists:
+                raise exc.DataError("", "", "", False, 0)
+            my_grid = Grid.query.filter_by(title=my_title).first()
+            my_grid.data = new_data
+            db.session.commit()
+            response = Response(my_json, status=HTTP_CREATED, mimetype='application/json') 
+        except exc.DataError:
+            db.session.rollback()
+            response = Response(my_json, status=HTTP_CONFLICT, mimetype='application/json')
+        finally:
+            db.session.close()
     return response
 
 @application.route('/api/v1/query-all-titles/', methods=['GET'])
 def get_all_titles():
     try:
-        titles_query = db.session.query(Grid.title).all()
-        all_titles = json.dumps(titles_query)
+        titles_json = json.dumps(db.session.query(Grid.title).all())
         db.session.expunge_all()
-        response = Response(all_titles, status=HTTP_OK, mimetype='application/json')
+        response = Response(titles_json, status=HTTP_OK, mimetype='application/json')
     except Exception as e:
         print e
         db.session.rollback()
@@ -100,27 +101,29 @@ def get_all_titles():
 
 @application.route('/api/v1/query-all/', methods=['GET'])
 def get_all():
-    result = ""
     try:
-        result = Grid.query.all()
+        grids_json = json.dumps(Grid.query.all())
         db.session.expunge_all()
+        response = Response(grids_json, status=HTTP_OK, mimetype='application/json')
     except:
         db.session.rollback()
+        response = Response(status=HTTP_BADREQUEST)
     finally:
         db.session.close()
-    return str(result)
+    return response
 
 @application.route('/api/v1/search-grid/<search_title>', methods=['GET'])
 def search_grid(search_title):
-    result = ""
     try:
-        result = Grid.query.filter(Grid.title == search_title).first()
+        result_json = json.dumps(Grid.query.filter(Grid.title == search_title).first())
         db.session.expunge_all()
+        response = Response(result_json, status=HTTP_OK, mimetype='application/json')
     except:
         db.session.rollback()
+        response = Response(status=HTTP_BADREQUEST)
     finally:
         db.session.close()
-    return str(result)
+    return response
 
 @application.route('/api/v1/delete-grid/<delete_title>', methods=['GET'])
 def delete_grid(delete_title):
