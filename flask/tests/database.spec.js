2e5dff858e1b0c10fcbd71b21214546d89de66bb
mouse111 <mandalamouse@gmail.com>
2018-11-17 17:48:36 -0600

    worked on database.js and database.spec.js

--- flask/tests/database.spec.js
+++ flask/tests/database.spec.js
@@ -9,16 +9,39 @@ describe('Database', function() {
     var invalidJSON2 = JSON.parse('{ "title" : "mytitle3", "data" : "" }');
     var invalidJSON3 = JSON.parse('{ "title" : "", "data" : "" }');
     var invalidJSON4 = JSON.parse('{ "notatitle" : "bad title", "notdata" : "bad data" }');
-    
-    it('should test given JSONs for title and data fields', function() {
+    var invalidJSON5 = JSON.parse('{ "title" : "atitle", "data" : 123456789 }');
+    var invalidJSON6 = JSON.parse('{ "title" : "anothertitle", "data" : 0}');
+
+
+
+    it('should test that a given JSON with nonempty title and data fields is treated as valid', function() {
         expect(Database.validJSON(validJSON1)).toBeTruthy();
         expect(Database.validJSON(validJSON2)).toBeTruthy();
         expect(Database.validJSON(validJSON3)).toBeTruthy();
-        
-        expect(Database.validJSON(invalidJSON1)).toBeFalsy();
+    })
+
+
+    it('should test that a given JSON has title and data fields', function(){
+        expect(Database.validJSON(invalidJSON4)).toBeFalsy();
+    })
+
+
+    it('should test that a given JSON does not have an empty data field', function() {
         expect(Database.validJSON(invalidJSON2)).toBeFalsy();
         expect(Database.validJSON(invalidJSON3)).toBeFalsy();
-        expect(Database.validJSON(invalidJSON4)).toBeFalsy();
-    });
+    })
+
+
+    it('should test that a given JSON does not have an empty title field', function(){
+        expect(Database.validJSON(invalidJSON1)).toBeFalsy();
+        expect(Database.validJSON(invalidJSON3)).toBeFalsy();
+    })
+
+
+    it('should test that a given JSON does not have an incorrectly filled data field', function() {
+        expect(Database.validJSON(invalidJSON5)).toBeFalsy();
+        expect(Database.validJSON(invalidJSON6)).toBeFalsy();
+    })
+
 
 });
