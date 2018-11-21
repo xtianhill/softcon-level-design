cb63dd9535706d30803eefbfa019df861a6d668c
Vesperan <emadigan@uchicago.edu>
2018-11-20 19:54:52 -0600

    fixed character.js syntax

--- flask/static/character.js
+++ flask/static/character.js
@@ -1,107 +1,137 @@
+/*
+|------------------------------------------------------------------------------
+| Character Class
+|------------------------------------------------------------------------------
+|
+| This file contains the Character prototype (the javascript equivalent of a
+| class). Character is the super of Player, NPC, and Enemy. It contains data
+| about character status (e.g. health, speed), as well as methods for moving
+| and interacting with the game's physics.
+|
+|------------------------------------------------------------------------------
+*/
+
 const Element = require('./element.js');
 const Vector = require('./utility.js');
-//data: point location, int health, bool status
-//function void dechealth
-
-
-/*Character Prototype
-Note: location is a vector with x and y*/
 
 function Character(loc, max, hea, stat, hbox, url, size, spd, mvspd, grav){
 
-    if((spd instanceof Vector) && (typeof mvspd === "number") && (typeof grav === "number")&&  (typeof stat === "boolean") && (typeof max === "number") && (typeof hea ==="number")){
+    if((spd instanceof Vector) && (typeof mvspd === "number") &&
+      (typeof grav === "number")&&  (typeof stat === "boolean") &&
+      (typeof max === "number") && (typeof hea ==="number")){
         Element.call(this, loc, url, size, hbox);
         this.maxHealth = max; //maximum health
-	    this.health=hea; //int health
-	    this.status =stat; //true for alive, false for dead
+	      this.health=hea; //int health
+	      this.status =stat; //true for alive, false for dead
         this.speed = spd; //for moving
         this.moveSpeed = mvspd; //tells how fast it moves
         this.gravity = grav;
     }
     else return {};
-
 }
 
 Character.prototype = Object.create(Element.prototype);
 
+/*
+|--------------------------------------------------------------------------
+| Getter and setter functions for the Character prototype (the javascript
+| version of class methods).
+|--------------------------------------------------------------------------
+*/
 
-/* Example for how to create a function for a prototype. this is the javascript version
- of creating a method within a class*/
-//decrement health function
-// not in iteration 1
-// Character.prototype.decHealth = function(amount){
-//  	//decrement health by amount
-// }
+// Getter for position
 Character.prototype.getPosition = function(){
 	return this.position;
 }
 
+// Setter for position
 Character.prototype.setPosition = function(pos){
 	if(pos instanceof Vector){
 		this.position = pos;
 	}
 }
 
+// Getter for maxHealth
 Character.prototype.getMaxHealth = function(){
     return this.maxHealth;
 }
 
+// Setter for maxHealth
 Character.prototype.setMaxHealth = function(amount){
     if(typeof amount === "number"){
         this.maxHealth = amount;
     }
 }
 
+// Getter for health
 Character.prototype.getHealth = function(){
     return this.health;
 }
 
+// Setter for health
 Character.prototype.setHealth = function(amount){
     if(typeof amount === "number"){
         this.health= amount;
     }
 }
 
+// Getter for status
 Character.prototype.getStatus = function(){
     return this.status;
 }
 
+// Setter for status
 Character.prototype.setStatus = function(s){
     if(typeof s === 'boolean'){
         this.status = s;
     }
 }
 
+// Getter for speed
 Character.prototype.getSpeed = function(){
     return this.speed;
 }
 
+// Setter for speed
 Character.prototype.setSpeed = function(spd){
     if(spd instanceof Vector){
         this.speed =spd;
     }
 }
 
+// Getter for movementSpeed
 Character.prototype.getMovementSpeed = function() {
     return this.moveSpeed;
 }
 
+// Setter for movementSpeed
 Character.prototype.setMovementSpeed = function(ms){
     if(typeof ms === "number"){
         this.moveSpeed = ms;
     }
 }
 
+// Getter for gravity
 Character.prototype.getGravity = function () {
     return this.gravity;
 }
 
+// Setter for gravity
 Character.prototype.setGravity = function(g) {
     if(typeof g === "number"){
         this.gravity = g;
     }
 }
 
+/*
+|--------------------------------------------------------------------------
+| Movement-related functions for the Character prototype, wherein much of
+| the physics calculations are performed.
+|--------------------------------------------------------------------------
+*/
+
+// Calculates the hypothetical next x position of a Character given a direction
+// in which the character is trying to move.
 Character.prototype.newXPos = function(step, dir) {
   this.speed.x = 0
   if (dir == "right") this.speed.x = this.moveSpeed;
@@ -112,6 +142,8 @@ Character.prototype.newXPos = function(step, dir) {
   return newPos;
 };
 
+// Checks if a hypothetical next x position of a Character is legal, and if so,
+// updates its position.
 Character.prototype.moveX = function(newPos, obstacle) {
   if(obstacle != null) {
       if(obstacle.getSolid() == 0){
@@ -123,6 +155,7 @@ Character.prototype.moveX = function(newPos, obstacle) {
      }
 }
 
+// Calculates the hypothetical next y position of a character based on gravity
 Character.prototype.newYPos = function(step) {
   this.speed.y += step * this.gravity;
 
@@ -131,22 +164,21 @@ Character.prototype.newYPos = function(step) {
   return newPos;
 };
 
+// Checks if a hypothetical next y position of a Character is legal, and if so,
+// updates its position; if it hits the ground stops its motion; if it is on
+// ground and jumps, updates its speed to allow the jump on the next game loop.
 Character.prototype.moveY = function(newPos, obstacle, up) {
   var jumpSpeed = 70;
   if(obstacle != null) {
-      if(obstacle.getSolid() == 1)
+      if(obstacle.getSolid() == 1){
           newPos.x = this.position.x
           if (up && this.speed.y > 0){
               this.speed.y = -jumpSpeed;
           } else
               this.speed.y = 0;
-        else
-            this.position=newPos;
-        }
-   else
-       this.position = newPos;
+      } else
+          this.position = newPos;
+   }
 };
-
-
 
 module.exports = Character;
