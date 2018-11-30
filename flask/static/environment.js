695be5fe6826cf6956400b53e0d3f7f5faa3312a
Hayley Gray <hayleygray@uchicago.edu>
2018-11-29 22:50:43 -0600

    Environment passing all

--- flask/static/environment.js
+++ flask/static/environment.js
@@ -12,6 +12,7 @@
 
 /*note: Environment has flag for whether its solid or not*/
 const Element = require('./element.js');
+const Effect = require('./effect.js');
 
 /*
 |------------------------------------------------------------------------------
@@ -19,7 +20,7 @@ const Element = require('./element.js');
 |------------------------------------------------------------------------------
 */
 function Environment(solid, pos, url, scale, hbox, eff){
-  if (solid == 1 || solid == 0) {
+  if (solid == 0 || solid == 1) {
       Element.call(this, pos, url, scale, hbox);
       this.solid = solid;
       this.effect = eff;
@@ -33,7 +34,7 @@ Environment.prototype = Object.create(Element.prototype);
 
 Environment.prototype.Environment = function(){
     Element.call(this, new vector(0,0), null, new vector(50,50), new vector (50,50));
-    this.solid= true;
+    this.solid= 1;
     this.effect = new Effect("heal", 2);;
 };
 
@@ -44,14 +45,15 @@ Environment.prototype.getSolid = function(){
 
 //Setter for solid
 Environment.prototype.setSolid = function(bool){
-  if (solid == 1 || solid == 0){
+  if (bool == 1 || bool == 0){
       this.solid = bool;
   }
 };
 
 //Setter for effect
 Environment.prototype.setEffect= function(eft){
-    this.effect = eft;
+    if(eft instanceof Effect)
+        {this.effect = eft;}
 };
 
 //Getter for effect
