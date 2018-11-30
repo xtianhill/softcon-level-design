d7239bda2ca2cd38abd9972b97315aa7d4ab5494
mouse111 <mandalamouse@gmail.com>
2018-11-29 21:56:04 -0600

    pushing character-browserify.spec.js and element-browserify.spec.js

--- flask/tests/element-browserify.spec.js
+++ flask/tests/element-browserify.spec.js
@@ -104,25 +104,32 @@ module.exports = Element;
 */
 function Vector(x,y){
 	if (typeof(x) != 'number' || typeof(y) != 'number'){
-		return null
+		return {};
 	}
-	this.x=x;
-	this.y=y;
+	else{
+		this.x=x;
+		this.y=y;
+	}
 }
 
 //Add to the vector
 Vector.prototype.plus = function(vec) {
-	return new Vector (this.x + vec.x, this.y + vec.y);
-}
-
-//Multiply the vector times a vector
-Vector.prototype.times = function(vec) {
-	return new Vector (this.x * vec.x, this.y * vec.y);
+	if (typeof(vec.x) != 'number' || typeof(vec.y) != 'number'){
+		return {};
+	}
+	else{
+		return new Vector (this.x + vec.x, this.y + vec.y);
+	}
 }
 
 //Multiply the vector times a number
 Vector.prototype.times = function(num) {
 	return new Vector (this.x * num, this.y * num);
+}
+
+//Multiply the vector times a vector
+Vector.prototype.times = function(vec) {
+	return new Vector (this.x * vec.x, this.y * vec.y);
 }
 
 module.exports = Vector;
