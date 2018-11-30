f37773326e00e02f4d49e87de24c8d6e543ce78f
Unknown <mollyodonnell@uchicago.edu>
2018-11-30 04:33:16 -0600

    asdf

--- flask/static/index.js
+++ flask/static/index.js
@@ -1797,13 +1797,12 @@ function JSONtoElements(data){
     console.log("data in paring", data);
     var dataobj= JSON.parse(data);
     console.log(dataobj);
-   dataobj.canvas;
     
     i=0;
     var elementarray= [];
     var backgroundurl= dataobj.background; //dataobj.backgroundImage";
-        for (i=0; i<dataobj.canvas.length; i++){
-            var temp= dataobj.canvas[i];
+        for (i=0; i<dataobj.objects.length; i++){
+            var temp= dataobj.objects[i];
             if (temp.type =="Element"){
                 var pos = new Vector(temp.left, temp.top);
                 var sz = new Vector(50,50);
@@ -1812,23 +1811,23 @@ function JSONtoElements(data){
                 var element;
                 if (temp.name == "Environment"){
                     
-                    var eff = new Effect(dataobj.canvas[i].effect, 1); // new Effect("damage", 1);
-                    var solid = dataobj.canvas[i].solid;
+                    var eff = new Effect(dataobj.objects[i].effect, 1); // new Effect("damage", 1);
+                    var solid = dataobj.objects[i].solid;
                     element = new Environment(solid,pos,url,sz,hitbox,eff);
                     console.log(element);
                 }
                 else if (temp.name == "Item"){
-                    console.log("item", dataobj.canvas[i]);
+                    console.log("item", dataobj.objects[i]);
                     var col = false;
                     var eff = new Effect("damage", 1);
                     var hov =true;
-                    var targets = dataobj.canvas[i].targets;
+                    var targets = dataobj.objects[i].targets;
                     element = new Item(pos, url, sz, hitbox, col, eff, pos, hov, targets);
                     console.log('from parsing');
                 }
                 else if (temp.name == "Player"){
-                    console.log("player",dataobj.canvas[i]);
-                    var max = 10;//dataobj.canvas[i].maxhealth;
+                    console.log("player",dataobj.objects[i]);
+                    var max = 10;//dataobj.objects[i].maxhealth;
                     var stat = true;
                     var itm= null;
                     var inv= [];
@@ -1836,8 +1835,8 @@ function JSONtoElements(data){
                     if(url === defaultUrl)
                         hitbox = new Vector(19,50);
                     var spd = new Vector(0,0);
-                    var mvspd = 30;//dataobj.canvas[i].speed;
-                    var grav =  50;//dataobj.canvas[i].gravity;
+                    var mvspd = 30;//dataobj.objects[i].speed;
+                    var grav =  50;//dataobj.objects[i].gravity;
                     var dir = "right";
                     element = new Player(pos, max, max, stat, itm, inv, hitbox, url, sz, spd, mvspd, grav, dir);
                 }
@@ -1845,7 +1844,7 @@ function JSONtoElements(data){
                     var max = 10;
                     var hea = 7;
                     var stat = true; 
-                    var msg =  dataobj.canvas[i].msg;
+                    var msg =  dataobj.objects[i].msg;
                     var spd = new Vector(0,0);
                     var mvspd = 30;
                     var grav = 50;
@@ -1855,7 +1854,7 @@ function JSONtoElements(data){
                     var max = 10;
                     var hea= 10;
                     var stat = true;
-                    var dmg =  1;//dataobj.canvas[i].damage;
+                    var dmg =  1;//dataobj.objects[i].damage;
                     var spd = new Vector(0,0);
                     var mvspd = 15;
                     var grav = 60;
